6de43912fe74e5f8903bf92bc2be5a7729b48a32
janfabian <jan.fabian@topmonks.com>
2018-06-05 04:30:56 +0200

    optional ssl, add db name

--- 3.4/backup.sh
+++ 3.4/backup.sh
@@ -8,9 +8,18 @@ DATE=$(date +%Y%m%d_%H%M%S)
 FILE="/backup/backup-$DATE.tar.gz"
 
 mkdir -p dump
-mongodump -h $MONGO_HOST --ssl \
-  --username $MONGO_USERNAME --password $MONGO_PASSWORD \
-  --authenticationDatabase admin
+
+args=(
+  --host "$MONGO_HOST"
+  --username "$MONGO_USERNAME"
+  --password "$MONGO_PASSWORD"
+  --db "$MONGO_DB"
+)
+if [[ -v MONGO_SSL ]]; then
+    args+=(--ssl)
+fi
+
+mongodump "${args[@]}"
 tar -zcvf $FILE dump/
 rm -rf dump/
 
